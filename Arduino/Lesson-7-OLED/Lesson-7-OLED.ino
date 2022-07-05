80b89c98841ed7d301e4a082bc2bc4454bb85363
Mieluoxxx <92699499+Mieluoxxx@users.noreply.github.com>
2022-07-05 15:47:38 +0800

    2022.7.5

--- Arduino/Lesson-7-OLED/Lesson-7-OLED.ino
+++ Arduino/Lesson-7-OLED/Lesson-7-OLED.ino
@@ -10,6 +10,6 @@ void setup(void) {
 
 void loop(void) {
   u8x8.setFont(u8x8_font_chroma48medium8_r);
-  u8x8.setCursor(0, 0);
-  u8x8.print("Hello World!");
+  u8x8.setCursor(4, 5);
+  u8x8.print("ZXD666");
 }
